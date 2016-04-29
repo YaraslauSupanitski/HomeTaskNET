ea1d819fd169fbd39f202f9238c8d04d081520c6
YaraslauSupanitski <Yaraslau_Supanitski@epam.com>
2016-04-29 17:38:08 +0300

    anfinisht task with linq

---
+++ ConApp5_3(NewTask)/LinqWorkerForCastumers.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConApp5_3_NewTask_
+{
+    class LinqWorkerForCastumers
+    {
+        private string _path;
+
+        private XDocument _xmlDoc;
+        private XElement _rootElement;
+
+
+
+        public LinqWorkerForCastumers(string path)
+        {
+            this._path = path;
+            this._xmlDoc = XDocument.Load(_path);
+            this._rootElement = _xmlDoc.Root;
+        }
+        
+        public void Task1()
+        {
+            Random rnd = new Random();
+            var maxOrder = _rootElement.Elements("customer")
+                    .Max(cust => cust.Element("orders").Elements("order").Count());
+
+            //decimal x = rnd.Next(10000, 100000);
+            decimal x = 20000;
+
+            Console.WriteLine($"Max Sum {x}");
+
+            var a = _rootElement.Elements("customer").
+                Where(cus => cus.Element("orders").Elements("order").
+                Sum(ord => Decimal.Parse(ord.Element("total").Value)) > x).
+                Select(cus => cus);
+
+            int o = 0;
+            foreach (var t in a)
+            {
+
+                Console.WriteLine(o++ + " " + t.Element("id").Value + " " + t.Element("orders").Elements("order").Sum(ord => Decimal.Parse(ord.Element("total").Value)));
+            }
+
+        }
+        
+        public void Task2()
+        {
+            var custByCountry = from customer in _rootElement.Elements("customer")
+                    group customer by customer.Element("country").Value;
+
+            foreach (var cust in custByCountry)
+            {
+                Console.WriteLine(cust.Key + ":");
+
+                foreach (var country in cust)
+                {
+                    Console.WriteLine("\t" + country.Element("id").Value);
+                }
+            }
+        }
+        
+        public void Task3(decimal price)
+        {
+            var custListWithPriceHighPrice = (from customer in _rootElement.Elements("customer")
+                    from order in customer.Element("orders").Elements()
+                    where Decimal.Parse(order.Element("total").Value) >= price
+                    select customer);
+
+
+            Console.WriteLine($"Prise: {price}");
+            foreach (var cust in custListWithPriceHighPrice.Distinct())
+            {
+                var maxPrice = cust.Element("orders").Elements()
+                    .Max(ord => Decimal.Parse(ord.Element("total").Value));
+
+                Console.WriteLine($"{cust.Element("id").Value} {maxPrice}");
+            }
+        }
+        
+        public void Task4()
+        {
+            var customerList = (from customer in _rootElement.Elements()
+                                from order in customer.Element("orders").Elements()
+                                group DateTime.Parse(order.Element("orderdate").Value) by customer
+                            );
+
+            foreach (var customer in customerList)
+            {
+                Console.Write($"{customer.Key.Element("id").Value}:\t");
+                Console.WriteLine(customer.First());
+            }
+        }
+        
+        public void Task5()
+        {
+            var customerList = from customer in _rootElement.Elements()
+                               from order in customer.Element("orders").Elements()
+                               orderby DateTime.Parse(order.Element("orderdate").Value),
+                                       customer.Element("id").Value
+                               group DateTime.Parse(order.Element("orderdate").Value) by customer into gr
+                               select new
+                               {
+                                   CusId = gr.Key.Element("id").Value,
+                                   data = gr.First(),
+                               };
+
+            foreach (var customer in customerList)
+            {
+                Console.Write($"{customer.CusId}:\t");
+                Console.WriteLine(customer.data);
+
+            }
+        }       
+        
+        public void Task6()
+        {
+            int i;
+            var resultListWithCus = from cus in _rootElement.Elements()
+                                    where String.IsNullOrEmpty((string)cus.Element("region"))
+                                          || !cus.Element("phone").Value.Contains('(')
+                                          || Int32.TryParse(cus.Element("postalcode").Value, out i)
+                                    select cus;
+            foreach (var custumer in resultListWithCus)
+            {
+                Console.WriteLine($"Id = {custumer.Element("id").Value}\t" +
+                                  $"region = { (string)custumer.Element("region")}\t" +
+                                  $"phone = { (string)custumer.Element("phone")}\t" +
+                                  $"postalcode = { (string)custumer.Element("postalcode")}\t");
+
+            }
+        }
+            
+        public void Task7()
+        {
+            var ruseltWithAveragePrise = 
+                from cus in _rootElement.Elements()
+                where cus.Element("orders")
+                                    .Elements()
+                                    .Any()
+                from order in cus.Element("orders").Elements()
+                group Decimal.Parse(order.Element("total").Value)   
+                                        by cus.Element("city").Value into gr
+                select new
+                {
+                    average = gr.Average(),
+                    city = gr.Key,
+                    number = gr.Count()
+                };
+
+
+            foreach (var el in ruseltWithAveragePrise)
+            {
+                Console.WriteLine("=========================");
+                Console.WriteLine($"City: {el.city}");
+                Console.WriteLine($"Average {el.average}");
+                Console.WriteLine($"THIS end {el.number}");
+            }
+        }
+        
+        }
+    }
+
--- ConApp5_3(NewTask)/Program.cs
+++ ConApp5_3(NewTask)/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -11,34 +12,85 @@ namespace ConApp5_3_NewTask_
     {
         static void Main(string[] args)
         {
+            //need add Log For out information.
+            //need shange void to list methods
             string path = $"{Environment.CurrentDirectory}\\Data\\Customers.xml";
 
-            XDocument xmlDoc = XDocument.Load(path);
-            var custs = xmlDoc.Element("customers");
-            
-            Random rnd = new Random();
-            var maxOrder = custs.Elements("customer")
-                    .Max(cust => cust.Element("orders").Elements("order").Count());
+            LinqWorkerForCastumers linqWorker = new LinqWorkerForCastumers(path);
 
-            decimal x = rnd.Next(10000, 100000);
-            
-            Console.WriteLine($"Max Sum {x}");
-            var a = custs.Elements("custome").
-                Where(cus => cus.Element("order").Elements("order").
-                Sum(ord => Decimal.Parse(ord.Attribute("total").Value)) > x).
-                Select(cus => cus);
+            //linqWorker.Task6();
+            //linqWorker.Task7();
+            //linqWorker.Task5();
+            //linqWorker.Task4();
+            //linqWorker.Task3(new decimal(2000));
+            //linqWorker.Task2();
+            //linqWorker.Task1();
 
-            foreach(var t in a)
+
+
+
+            //Nedd add to method. Its hardcode task8
+            /*
+            var tttw = 
+                from cus in custs.Elements()
+                      from order in cus.Element("orders").Elements()
+                      let date = ((DateTime)order.Element("orderdate")).Month
+                      let price = decimal.Parse((string)order.Element("total"))
+                orderby date
+                group price by date into gr
+                    select new
+                {
+                    year = gr.Key,
+                    average = gr.Average()
+                };
+
+            */
+            /*
+            foreach (var c in tttw)
             {
-                Console.WriteLine(t);
+                Console.WriteLine("========================================");
+                Console.WriteLine($"Year: {c.year}; Average: {c.average}");
+
             }
+            */
+
+            //foreach(var t in ttt)
+            //{
+            //    Console.Write(t.Key);
+            //    foreach(var tt in t)
+            //    {
+            //    }
+            //}
+
+
 
             /*
-            a.ToList().ForEach(cus => Console.WriteLine($"{cus.CustomerID} {cus.Orders.Sum(ord => ord.Total)}"));
-            */
+                      var wwww = from cus in custs.Elements()
+                                 from order in cus. 
+                                                  Element("orders").Elements()
+                                 group order by cus into orderGroup  //kay = cus' orderЫ = value
+                                          from yearGroup in
+                                                      (from order in orderGroup //year <= order
+                                                      group order by (DateTime.Parse(order.Element("orderdate").Value)).Year)
+                                                      //kay = year value = orders
+                                                      group yearGroup by orderGroup.Key;
+
+                      foreach (var c in wwww)
+                      {
+                          Console.WriteLine("========================================");
+                          Console.WriteLine($"Customer: {(string)c.Key.Element("id")}");
+                          foreach (var item in c)
+                          {
+                              Console.WriteLine($"Year: {item.Key}");
+                              Console.WriteLine($"Total: {item.Sum(el => ((decimal)el.Element("total")))}");
+                          }
+                      }
+
+              */
+
             Console.Read();
-
-
+            }
         }
     }
-}
+
+
